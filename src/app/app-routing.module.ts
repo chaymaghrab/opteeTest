dfa6b4037f3fd3e7d6267739c79c8aeb4a200ab7
chaymaghrab <ghratrichayma@gmail.com>
2023-08-07 23:47:49 +0200

    commit2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageAccueilComponent } from './page-accueil/page-accueil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PageAccueilComponent },
+  { path: 'error/:errorCode', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/error/404' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
